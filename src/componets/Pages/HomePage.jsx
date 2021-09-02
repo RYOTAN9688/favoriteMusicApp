97e8c78fa3366d4874fbe6a765d5caf4e887ad76
RYOTAN9688 <ryota8813@gmail.com>
2021-09-02 22:46:17 +0900

    first commit

---
+++ src/componets/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>FavoriteMusic</h1>
+      <h2>TopPage</h2>
+    </div>
+  )
+}
---
+++ src/componets/Pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>FavaritePage</h1>
+      <h2>Login</h2>
+    </div>
+  )
+}
