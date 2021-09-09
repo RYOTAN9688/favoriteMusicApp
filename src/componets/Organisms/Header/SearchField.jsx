30d3d096b2c957457c6bac5dacc13debcaae04ec
RYOTAN9688 <ryota8813@gmail.com>
2021-09-09 20:31:38 +0900

    Spotify Login

--- src/componets/Organisms/Header/SearchField.jsx
+++ src/componets/Organisms/Header/SearchField.jsx
@@ -2,28 +2,37 @@ import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import { Button } from '@material-ui/core'
 
-export const SearchField = () => {
+const SearchField = ({ defaultValue, onChange, onSubmit }) => {
   return (
     <div>
-      <TextField variant="outlined" id="searhMusic" label="検索"></TextField>
-      <Button type="button" onClick="">
+      <TextField
+        variant="outlined"
+        id="searhMusic"
+        label="検索"
+        defaultValue={defaultValue}
+        onChange={onChange}
+      ></TextField>
+      <Button type="button" onClick={onSubmit}>
         Search
       </Button>
     </div>
   )
 }
 
-export const SearchFormContainer = ({
-  className,
-  defaultValue,
-  onSubmit,
-  presenter,
-}) => {
+const SearchFormContainer = ({ defaultValue, onSubmit, presenter }) => {
   const [keyword, setKeyword] = useState(defaultValue)
+  console.log(keyword)
   return presenter({
-    className,
     defaultValue,
     onChange: setKeyword,
     onSubmit: () => onSubmit(keyword),
   })
 }
+
+SearchFormContainer.defalutProps = {
+  defaultValue: '',
+}
+
+export const SearchForm = props => (
+  <SearchFormContainer presenter={SearchField} {...props} />
+)
